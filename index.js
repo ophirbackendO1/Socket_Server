96c71613d8be51343a4d253b6ee10ef2e5f31e84
ophirbackend01 <ophir.backend01@gmail.com>
2024-11-13 09:26:54 +0630

    test

--- index.js
+++ index.js
@@ -1,110 +1,154 @@
+// const http = require("http");
+// const socketIo = require("socket.io");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+
+// const app = express();
+
+// app.use(bodyParser.json());
+
+// const server = http.createServer();
+// const io = socketIo(server, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
+
+// host = "192.168.100.13";
+
+// io.on("connection", (socket) => {
+//   console.log(
+//     "An user is connecting to the server with socket_id : " + socket.id
+//   );
+
+//   socket.on("message", (data) => {
+//     io.emit("message", data);
+//   });
+
+//   socket.on("order-event", (data) => {
+//     console.log("An user placed an order now!");
+//     io.emit("order-event", data);
+//   });
+
+//   socket.on("table-event", (data) => {
+//     console.log("Table status changed!");
+//     io.emit("table-event", data);
+//   });
+
+//   socket.on("migrate-table-event", (data) => {
+//     console.log("Table status changed!");
+//     io.emit("migrate-table-event", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("An user is disconnected");
+//   });
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.get("/ping", (req, res) => {
+//   res.send("pong");
+// });
+
+// app.post("/order-event", (req, res) => {
+//   res.json({ status: "Event emitted successfully" });
+//   // const event = req.body.event;
+//   // const data = req.body.data;1
+
+//   // io.emit(event, data);
+//   // io.emit("table-event", data);
+
+//   // console.log("An user placed an order now!");
+
+//   // res.json({ status: "Event emitted successfully", event: event, data });
+// });
+
+// app.post("/table-event", (req, res) => {
+//   const event = req.body.event;
+//   const data = req.body.data;
+
+//   io.emit("table-event", data);
+
+//   console.log("A " + data.old_status + " table is " + data.status + " now!");
+
+//   res.json({ status: "Event emitted successfully", event: event, data });
+// });
+
+// app.post("/migrate-table-event", (req, res) => {
+//   const event = req.body.event;
+//   const data = req.body.data;
+
+//   io.emit("migrate-table-event", data);
+
+//   console.log(
+//     "Orders from table_id " +
+//       data.old_table_id +
+//       " migrated to table_id " +
+//       data.new_table_id
+//   );
+
+//   res.json({ status: "Event emitted successfully", event: event, data });
+// });
+
+// app.listen(4000, () => {
+//   console.log("Api server running at port 4000");
+// });
+
+// server.listen(3000,host, () => {
+//   console.log("Socket server running at port 3000");
+// });
+
+// // app.listen(4000, () => {
+// //   console.log("Api server running at 127.0.0.1:4000");
+// // });
+
+// // server.listen(3000, host, () => {
+// //   console.log("Socket server running at " + host + ":3000");
+// // });
+
 const http = require("http");
-const socketIo = require("socket.io");
 const express = require("express");
-const bodyParser = require("body-parser");
+const socketIo = require("socket.io");
 
 const app = express();
-
-app.use(bodyParser.json());
-
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "*", // Adjust this to restrict to certain origins if needed
   },
 });
 
-host = "0.0.0.0";
+// Middleware
+app.use(express.json()); // To parse JSON bodies in requests
 
+// Set up Socket.IO
 io.on("connection", (socket) => {
-  console.log(
-    "An user is connecting to the server with socket_id : " + socket.id
-  );
-
-  socket.on("message", (data) => {
-    io.emit("message", data);
-  });
+  console.log("User connected:", socket.id);
 
   socket.on("order-event", (data) => {
-    console.log("An user placed an order now!");
+    console.log("Order event received");
     io.emit("order-event", data);
   });
 
-  socket.on("table-event", (data) => {
-    console.log("Table status changed!");
-    io.emit("table-event", data);
-  });
-
-  socket.on("migrate-table-event", (data) => {
-    console.log("Table status changed!");
-    io.emit("migrate-table-event", data);
-  });
-
   socket.on("disconnect", () => {
-    console.log("An user is disconnected");
+    console.log("User disconnected:", socket.id);
   });
 });
 
+// Routes for Express
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World from Express and Socket.IO!");
 });
 
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-app.post("/order-event", (req, res) => {
-  res.json({ status: "Event emitted successfully" });
-  // const event = req.body.event;
-  // const data = req.body.data;1
-
-  // io.emit(event, data);
-  // io.emit("table-event", data);
-
-  // console.log("An user placed an order now!");
-
-  // res.json({ status: "Event emitted successfully", event: event, data });
+// Start the server
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-app.post("/table-event", (req, res) => {
-  const event = req.body.event;
-  const data = req.body.data;
-
-  io.emit("table-event", data);
-
-  console.log("A " + data.old_status + " table is " + data.status + " now!");
-
-  res.json({ status: "Event emitted successfully", event: event, data });
-});
-
-app.post("/migrate-table-event", (req, res) => {
-  const event = req.body.event;
-  const data = req.body.data;
-
-  io.emit("migrate-table-event", data);
-
-  console.log(
-    "Orders from table_id " +
-      data.old_table_id +
-      " migrated to table_id " +
-      data.new_table_id
-  );
-
-  res.json({ status: "Event emitted successfully", event: event, data });
-});
-
-// app.listen(4000, () => {
-//   console.log("Api server running at port 4000");
-// });
-
-server.listen(3000, () => {
-  console.log("Socket server running at port 3000");
-});
-
-// app.listen(4000, () => {
-//   console.log("Api server running at 127.0.0.1:4000");
-// });
-
-// server.listen(3000, host, () => {
-//   console.log("Socket server running at " + host + ":3000");
-// });
